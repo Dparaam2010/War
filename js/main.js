f337c8146e98623f8670cf376e076a5c102d333f
Dparaam2010 <dparaam@gmail.com>
2023-08-07 12:01:41 -0700

    html, little css, make deck

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+/*----- constants -----*/
+const CARDS = [ 
+    'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','K♣','Q♣',
+    'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','K♦','Q♦',
+    'A♥️','2♥️','3♥️','4♥️','5♥️','6♥️','7♥️','8♥️','9♥️','10♥️','J♥️','K♥️','Q♥️',
+    'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','K♠','Q♠',
+]
+
+
+// /*----- state variables -----*/
+
+
+
+// /*----- cached elements  -----*/
+
+
+
+// /*----- event listeners -----*/
+
+
+// /*----- functions -----*/
+function shuffle(arr) {
+    const newArray = [...arr]
+    const length = newArray.length
+    for (let start = 0; start < length; start++) {
+      const randomPosition = Math.floor((newArray.length - start) * Math.random())
+      const randomItem = newArray.splice(randomPosition, 1)
+      newArray.push(...randomItem)
+    }
+    return newArray
+  }
+
