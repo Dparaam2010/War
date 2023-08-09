3063fb604f0c252ae9efad739d3b777baaad2efa
Dparaam2010 <dparaam@gmail.com>
2023-08-09 16:57:24 -0700

    download card images and attached each image to each card

--- js/main.js
+++ js/main.js
@@ -31,10 +31,10 @@ const CARDS = [
     const CARDVALUES = [{'2':2}, {'3':3}, {'4':4}, {'5':5}, {'6':6},{'7':7},{'8':8},{'9':9},{'10':10},{'J':11},{'Q':12},{'K':13},{'A':14}]
    
     const CARDIMAGES = {
-       'A♣' : 'images/clubs/clubs-A.svg',//'2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','K♣','Q♣',
-      // 'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','K♦','Q♦',
-      // 'A♥️','2♥️','3♥️','4♥️','5♥️','6♥️','7♥️','8♥️','9♥️','10♥️','J♥️','K♥️','Q♥️',
-      // 'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','K♠','Q♠',
+       'A♣' : 'images/clubs/clubs-A.svg','2♣' : 'images/clubs/clubs-r02.svg','3♣' : 'images/clubs/clubs-r03.svg' ,'4♣' : 'images/clubs/clubs-r04.svg','5♣' : 'images/clubs/clubs-r05.svg','6♣' : 'images/clubs/clubs-r06.svg','7♣' : 'images/clubs/clubs-r07.svg','8♣' : 'images/clubs/clubs-r08.svg','9♣' : 'images/clubs/clubs-r09.svg','10♣' : 'images/clubs/clubs-r10.svg','J♣' : 'images/clubs/clubs-J.svg','K♣' : 'images/clubs/clubs-K.svg','Q♣' : 'images/clubs/clubs-Q.svg',
+      'A♦' : 'images/diamonds/diamonds-A.svg','2♦' : 'images/diamonds/diamonds-r02.svg','3♦' : 'images/diamonds/diamonds-r03.svg','4♦' : 'images/diamonds/diamonds-r04.svg','5♦' : 'images/diamonds/diamonds-r05.svg','6♦' : 'images/diamonds/diamonds-r06.svg','7♦' : 'images/diamonds/diamonds-r07.svg','8♦' : 'images/diamonds/diamonds-r08.svg','9♦' : 'images/diamonds/diamonds-r09.svg','10♦' : 'images/diamonds/diamonds-r10.svg','J♦' : 'images/diamonds/diamonds-J.svg','K♦' : 'images/diamonds/diamonds-K.svg','Q♦' : 'images/diamonds/diamonds-Q.svg',
+      'A♥️' : 'images/hearts/hearts-A.svg','2♥️' : 'images/hearts/hearts-r02.svg','3♥️' : 'images/hearts/hearts-r03.svg','4♥️' : 'images/hearts/hearts-r04.svg','5♥️' : 'images/hearts/hearts-r05.svg','6♥️' : 'images/hearts/hearts-r06.svg','7♥️' : 'images/hearts/hearts-r07.svg','8♥️' : 'images/hearts/hearts-r08.svg','9♥️' : 'images/hearts/hearts-r09.svg','10♥️' : 'images/hearts/hearts-r10.svg','J♥️' : 'images/hearts/hearts-J.svg','K♥️' : 'images/hearts/hearts-K.svg','Q♥️' : 'images/hearts/hearts-Q.svg',
+      'A♠' : 'images/spades/spades-A.svg','2♠': 'images/spades/spades-r02.svg','3♠' : 'images/spades/spades-r03.svg','4♠' : 'images/spades/spades-r04.svg','5♠' : 'images/spades/spades-r05.svg','6♠' : 'images/spades/spades-r06.svg','7♠' : 'images/spades/spades-r07.svg','8♠' : 'images/spades/spades-r08.svg','9♠' : 'images/spades/spades-r09.svg','10♠' : 'images/spades/spades-r10.svg','J♠' : 'images/spades/spades-J.svg','K♠' : 'images/spades/spades-K.svg','Q♠' : 'images/spades/spades-Q.svg',
     }
     
 
