25a6a8e20bbe6cc44cfec2054ea1ba689b7d8298
Dparaam2010 <dparaam@gmail.com>
2023-08-10 13:50:51 -0700

    creating a branch

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ console.log('test')
 
 
 // i used the variable const to creat a array for all my suits and faces.
-const FACES = [ 
+const RANK = [ 
   'A','02','03','04','05','06','07','08','09','10','J','K','Q',
 ]
 
 const SUITS = ['d','h','c','s']
+
+const FACEVALUES = {'A' : 1,'J' : 11,'Q' : 12,'K' : 13,}
 
 
 // /*----- state variables -----*/
@@ -19,10 +21,10 @@ let winner, plAScore, plBScore, playerADeck, playerBDeck, turnNum, playerACardVa
 const DECK = []
 
 // /*----- cached elements  -----*/
-const cardc06 = document.querySelector(".card c06");
-const cardd09 = document.querySelector(".card d09");
-const plAScores = document.querySelector(".pla-score");
-const plBScores = document.querySelector(".plab-score");
+const cardc06 = document.querySelector(".cardc06");
+const cardd09 = document.querySelector(".cardd09");
+const plAScores = document.getElementById("pla-score");
+const plBScores = document.getElementById("plab-score");
 
 const messageEl = document.querySelector("h2")
 
@@ -43,19 +45,13 @@ playAgnButton.addEventListener("click", init);
 
 init();
 
-function generateDeck() {
-  SUITS.forEach(s=>{
-    FACES.forEach(f=>{
-      DECK.push(
-        {face:s+f}
-      )
-    })
-  })
-}
 
 function init() {
+  if (DECK.length !== 52) {
+    generateDeck()
+
+  }
   // split the deck into two decks for Player A and B
-  generateDeck()
   console.log(DECK)
   const shuffleDeck = shuffle(DECK)
   playerADeck = shuffleDeck.slice(0, 26)
@@ -63,55 +59,102 @@ function init() {
   winner = null;
   plAScore = 0;
   plBScore = 0;
-  // messageEl.innerText = "PLAYER A VS PLAYER B "
-
+  messageEl.innerText = "PLAYER A VS PLAYER B "
 }
-
-function render() {
-  // renderBoard()
-  renderScore()
-  renderControls()
-}
-
-function renderScore() {
-  plAScores.innerText = plAScore
-  plBScores.innerText = plBScore
-}
-//Ternary Operator
-function renderControls() {
-  playAgnButton.style.visibility = winner ? "visable" : "hidden"
-}
-//if we get to 26 that = getWinner
-function handleDeal() {
-  if (turnNum === 26) getWinner()
-  // plACards.style.backgroundImage = CARDIMAGES[playerADeck[turnNum]]
-  // plBCards.style.backgroundImage = CARDIMAGES[playerBDeck[turnNum]]
-  let playerACardVal = DECK[playerADeck[turnNum]]
-  let cardd09Val = DECK[playerBDeck[turnNum]]
-  compareCards(cardc06, cardc06Val)
-  render()
+  
+  function render() {
+    // renderBoard()
+    renderScore()
+    renderControls()
   }
+  
+  function renderScore() {
+    plAScores.innerText = plAScore
+    plBScores.innerText = plBScore
+  }
+  //Ternary Operator
+  function renderControls() {
+    playAgnButton.style.visibility = winner ? "visable" : "hidden"
+  }
+  //if we get to 26 that = getWinner
+  function handleDeal() {
+    if (turnNum === 26) getWinner()
+    let cardc06 = playerADeck[turnNum]
+    let cardd09 = playerBDeck[turnNum]
+  compareCards(cardc06.value, cardd09.value)
+  render()
+}
+//DECK[0].face
 //if playerA is === to player B that its war, other wise if player A is less than player B then increase playerB by one, otherwise if 
 //player A is greater than player B than player A incease by 1
-  function compareCards(cardc06,cardd09Val) {
-    if (cardc06Val === cardd09Val) {
-      messageEl.innerText = "IT'S WAR"
-      turnNum++}
-    else if(cardc06Val < cardd09Val) {
+function compareCards(cardc06Val,cardc09Val) {
+  if (cardc06Val === cardc09Val) {
+    messageEl.innerText = "IT'S WAR"
+    turnNum++}
+    else if(cardc06Val < cardc09Val) {
       messageEl.innerText = ""
       plBScore++
       turnNum++}
-      else if(cardc06Val > cardd09Val) {
+      else if(cardc06Val > cardc09Val) {
         messageEl.innerText = ""
         plAScore++
         turnNum++}
-  }
+      }
+      function generateDeck() {
+        SUITS.forEach(s=>{
+          RANK.forEach(r=>{
+            if(r <= 10) {
+              DECK.push({
+                face: `${s}${r}`,
+                value: Number(r)}) }
+              else if(RANK === 'J') {
+                DECK.push({
+                  face: `${s}${r}`,
+                  value: 11})}
+                else if(RANK === 'Q'){
+               DECK.push({
+                face: `${s}${r}`,
+                value: 12})}
+                else if (RANK === 'K'){
+                  DECK.push({
+                    face: `${s}${r}`,
+                    value: 13})}
+                else if (RANK ==='A'){
+                    DECK.push({
+                      face: `${s}${r}`,
+                      value: 14})}})
+        })
+      }
 
+      // A of diamonds
+      // {
+      //   face: 'dA'
+      //   value: 14
+      // }
+      // 2 of diamonds
+      // {
+      //   face: 'd02'
+      //   value: 2
+      // }
+      // 3 of diamonds
+      // {
+      //   face: 'd03'
+      //   value: 3
+      // }
+      // 4 of diamonds
+      //   {
+      //   face: 'd04'
+      //   value: 4
+      // }
+      // 5 of diamonds
+      // {
+      //   face: 'd05'
+      //   value: 5
+      // }
 
-  
-  
-
-
+   
+     
+      
 // if player A's score === player B's score there's no Winner, other wise if player A greater than player B than Player A win's
 // other wise if player A's score is less than player B's score than player B wins
 function getWinner() {
@@ -126,13 +169,6 @@ function getWinner() {
   }
 }
 
-
-
-
-
-
-
-
 // this function will shuffle the deck
 
 function shuffle(arr) {
@@ -145,4 +181,5 @@ function shuffle(arr) {
     }
     return newArray
   }
+
 
